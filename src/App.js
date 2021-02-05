af8fc5e8c287ed74091a13ef7576b004b28293ca
b0nanze <iumkhaev@gmail.com>
2021-02-06 00:51:48 +0300

    do

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    const [todos, setTodos] = useState([
+        {
+            text: "Сделать работу",
+            id: 1
+        },
+        {
+            text: "Посмотреть телевизор",
+            id: 2
+        },
+        {
+            text: "Сдать работу",
+            id: 3
+        }
+    ]);
+    const deleteTodo = (checkIn) => {
+        const filter = todos.filter((e, index) => {
+            return index !== checkIn;
+        });
+        setTodos(filter)
+    }
+
+    const newTodos = todos.map((item, index) => {
+        return(
+            <div>
+                <div className="work">
+                    <div>{item.text}</div>
+                    <input type="checkbox"/>
+                    <button onClick={() => deleteTodo(index)}>❌</button>
+                </div>
+            </div>
+        )
+        });
+
+    const [text, setText] = useState("");
+
+    const addTodos = (item, index) => {
+        setTodos([{
+            text:text,
+            id: index
+        }, ...todos])
+        setText("");
+
+    }
+    const handleChange = (e) => {}
+
+    return (
+        <div className="app">
+            <div className="header">
+                <h1>Список Дел</h1>
+            </div>
+            <hr />
+            <div className="form">
+            <input placeholder="Введите текст..." type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+            <input type="checkbox"/>
+            <button onClick={addTodos}>Добавить</button>
+            </div>
+            <div>{newTodos}</div>
+        </div>
+    );
 }
 
 export default App;
